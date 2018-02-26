90bfbcc7100edab89926131154065484573c52a7
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2018-02-26 22:24:42 +0000

    Add Numeral.js

--- dist/script.js
+++ dist/script.js
@@ -4,26 +4,10 @@ let historyDiv = document.getElementById('history');
 const view = {
   total: '',
   addUserInputToInput(value) {
-    inputDiv.innerText += value;
-    inputDiv.innerText = Number(inputDiv.innerText.replace(/,/gi, '')).toLocaleString();
+    inputDiv.innerText = numeral(input.innerText + value).format('0,0');
   },
   addUserInputToHistory(value) {
-
-    // Get last input string from history
-    // Remove commas
-    // Apply new value
-    // Reapply commas
-    // Add to history
-    if (!!historyDiv.innerText.match(/[+×-÷]/g)) {
-      let lastInput = historyDiv.innerText.match(/(\d*,)*\d*$/)[0];
-      lastInput = lastInput.replace(/,/gi, '');
-      lastInput += value;
-      lastInput = Number(lastInput).toLocaleString();
-      historyDiv.innerText = historyDiv.innerText.replace(/(\d*,)*\d*$/, String(lastInput));
-    } else {
-      historyDiv.innerText += value;
-      historyDiv.innerText = Number(historyDiv.innerText.replace(/,/gi, '')).toLocaleString();
-    }
+    historyDiv.innerText += value;
   },
   clearAll() {
     inputDiv.innerText = '0';
@@ -32,9 +16,9 @@ const view = {
 };
 
 const utility = {
-  // wasOperatorPressedLast () {
-  //   // return !calculator.history.match(/\d$/);
-  // }
+  wasOperatorPressedLast() {
+    return !historyDiv.innerText.match(/\d$/);
+  },
   digitPressHandler(value) {
     // Is the input 0, as well as the value?
     // Then state is clean, so reject to avoid leading zeros.
@@ -51,12 +35,20 @@ const utility = {
     view.addUserInputToHistory(value);
     return true;
   },
+  operatorPressedHandler(value) {
+    view.addUserInputToHistory(value);
+  },
+  operators: ['add', 'subtract', 'divide', 'multiply'],
+  operatorsSymbols: ['+', '×', '-', '÷'],
 };
 
 document.getElementById('calculator__bottom').addEventListener('click', (e) => {
   const value = e.target.innerText;
+
   if (Number(value) || value === '0' || value === '00') {
     utility.digitPressHandler(value);
+  } else if (utility.operators.includes(e.target.value)) {
+    utility.operatorPressedHandler(value);
   } else if (value === 'C') {
     view.clearAll();
   }
@@ -64,3 +56,23 @@ document.getElementById('calculator__bottom').addEventListener('click', (e) => {
 
 // inputDiv.innerText += value;
 //     inputDiv.innerText = Number(inputDiv.innerText.replace(/,/gi, '')).toLocaleString();
+
+// else if (utility.operators.includes(value)) {
+//   historyDiv.innerText += value;
+// }
+
+// if (historyDiv.innerText.match(/[+×-÷]/g) && (Number(value) || value === '0' || value === '00')) {
+//   let lastInput = historyDiv.innerText.match(/(\d*,)*\d*$/)[0];
+//   lastInput = lastInput.replace(/,/gi, '');
+//   lastInput += value;
+//   lastInput = Number(lastInput).toLocaleString();
+//   historyDiv.innerText = historyDiv.innerText.replace(/(\d*,)*\d*$/, String(lastInput));
+// } else if (utility.operatorsSymbols.includes(value)) {
+//   if (utility.wasOperatorPressedLast()) {
+//     historyDiv.innerText = historyDiv.innerText.slice(0, -1);
+//   }
+//   historyDiv.innerText += value;
+// } else {
+//   historyDiv.innerText += value;
+//   // historyDiv.innerText = Number(historyDiv.innerText).toLocaleString();
+// }
