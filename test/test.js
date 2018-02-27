fc3ba48f6ce83adeb63164b0f832ad0b54f1f2ff
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2018-02-27 11:46:35 +0000

    Remove Mochawesome

--- test/test.js
+++ test/test.js
@@ -1,7 +1,33 @@
-describe('Edge Cases', () => {
-  const total = document.getElementById('input');
+/* global
+describe
+beforeEach
+view
+it
+expect
+:true */
+/* eslint no-undef: "error" */
 
-  it('Should display 0 as input on load.', () => {
-    expect(total.innerText).to.equal("0");
+
+const input = document.getElementById('input');
+const history = document.getElementById('history');
+
+describe('Edge Cases', () => {
+
+  beforeEach(() => {
+    view.clearAll();
+  });
+
+  it('Should display 0 as default on load.', () => {
+    expect(input.innerText).to.equal('0');
+  });
+
+  it('Should remove leading 0 on user input.', () => {
+    document.querySelectorAll("[value='1']")[0].click();
+    expect(input.innerText).to.equal('1');
+  });
+
+  it('Should reject adding a leading operator to history.', () => {
+    document.querySelectorAll("[value='add']")[0].click();
+    expect(history.innerText).to.equal('');
   });
 });
