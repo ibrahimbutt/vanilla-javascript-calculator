10195454b5851025ca08d0e152069d130e2c2278
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2018-03-03 19:51:38 +0000

    Add animations

--- dist/script.js
+++ dist/script.js
@@ -4,85 +4,35 @@
 /* eslint no-undef: "error" */
 /* eslint no-use-before-define: 0 */
 
-let inputDisplay = document.getElementById('input');
-let historyDisplay = document.getElementById('history');
-const buttonsSection = document.getElementById('calculator__bottom');
+// let inputDisplay = document.getElementById('input');
+// let historyDisplay = document.getElementById('history');
 
-let total = '';
-let wasOperatorPressedLast = false;
-let lastOperatorPressed = '';
-let equalWasPressedLast = false;
 
-const calculator = {
-  add() {
-    return Number(total) + Number(inputDisplay.innerText);
-  },
-  subtract() {
-    return Number(total) - Number(inputDisplay.innerText);
-  },
-  multiply() {
-    return Number(total) * Number(inputDisplay.innerText);
-  },
-  divide() {
-    return Number(total) / Number(inputDisplay.innerText);
-  },
-  getTotal() {
-    total = this[lastOperatorPressed]();
-    historyDisplay.innerText += inputDisplay.innerText;
-    inputDisplay.innerText = total;
-    equalWasPressedLast = true;
-  },
-  clearAll() {
-    total = '';
-    wasOperatorPressedLast = false;
-    lastOperatorPressed = '';
-    inputDisplay.innerText = '0';
-    historyDisplay.innerText = '';
-  },
+const clearAll = (button) => {
+  button.childNodes[1].classList.add('active');
+  button.classList.add('active');
+  setTimeout(() => {
+    button.childNodes[1].classList.remove('active');
+    button.classList.remove('active');
+  }, 750);
 };
 
 const onDigitPress = (button) => {
-  equalWasPressedLast = false;
-  if (wasOperatorPressedLast) {
-    inputDisplay.innerText = button.innerText;
-    wasOperatorPressedLast = false;
-  } else if (
-    button.value === '00' ||
-    button.value === '0' ||
-    (button.value === 'decimal' && inputDisplay.innerText === '0') ||
-    (inputDisplay.innerText.includes('.') && button.value === 'decimal')
-  ) {
-    // reject leading 0 and decimals, as well as multiple decimals
-    return false;
-  } else if (inputDisplay.innerText === '0') {
-    inputDisplay.innerText = button.innerText;
-  } else {
-    inputDisplay.innerText += button.innerText;
-  }
+  button.childNodes[1].classList.add('active');
+  button.classList.add('active');
+
+  setTimeout(() => {
+    button.childNodes[1].classList.remove('active');
+    button.classList.remove('active');
+  }, 750);
 };
 
 const onOperatorPress = (button) => {
-  wasOperatorPressedLast = true;
+  button.childNodes[1].classList.add('active');
+  button.classList.add('active');
 
-  if (inputDisplay.innerText.endsWith('.')) {
-    inputDisplay.innerText = inputDisplay.innerText.slice(0, -1);
-  }
-
-  if (inputDisplay.innerText === '0') {
-    // Reject Leading Operators
-    return false;
-  } else if (equalWasPressedLast) {
-    historyDisplay.innerText += button.innerText;
-    lastOperatorPressed = button.value;
-    return true;
-  } else if (total === '') {
-    total = inputDisplay.innerText;
-  } else {
-    total = calculator[lastOperatorPressed]();
-  }
-  lastOperatorPressed = button.value;
-
-  historyDisplay.innerText = historyDisplay.innerText + inputDisplay.innerText + button.innerText;
-  inputDisplay.innerText = total;
-  return true;
+  setTimeout(() => {
+    button.childNodes[1].classList.remove('active');
+    button.classList.remove('active');
+  }, 750);
 };
