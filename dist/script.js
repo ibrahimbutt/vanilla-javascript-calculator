963df3f20fe595fdd808dfdbed9f6dbafbdfeec6
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2018-03-08 19:51:24 +0000

    Redo Structure

--- dist/script.js
+++ dist/script.js
@@ -1,18 +1,139 @@
 'use strict';
 
-// import shuntingYard from './shuntingYard';
-// import postfixCalculator from './postfix';
+// Animation Activation
+var buttonPop = function buttonPop(button) {
+  button.classList.add('active');
+  setTimeout(function () {
+    button.classList.remove('active');
+  }, 750);
+};
 
-// const userInput = ['1', '+', '11', '*', '53'];
-// const outputQueue = shuntingYard(userInput);
-// const result = postfixCalculator(outputQueue);
+// Operator Map for algorithms
+var operatorMap = {
+  '^': {
+    value: 3,
+    association: 'right',
+    operate: function operate(leftOperand, rightOperand) {
+      return Math.pow(leftOperand, rightOperand);
+    }
+  },
+  '*': {
+    value: 3,
+    association: 'left',
+    operate: function operate(leftOperand, rightOperand) {
+      return leftOperand * rightOperand;
+    }
+  },
+  '/': {
+    value: 3,
+    association: 'left',
+    operate: function operate(leftOperand, rightOperand) {
+      return leftOperand / rightOperand;
+    }
+  },
+  '+': {
+    value: 2,
+    association: 'left',
+    operate: function operate(leftOperand, rightOperand) {
+      return Number(leftOperand) + Number(rightOperand);
+    }
+  },
+  '-': {
+    value: 2,
+    association: 'left',
+    operate: function operate(leftOperand, rightOperand) {
+      return leftOperand - rightOperand;
+    }
+  },
+  '(': {
+    value: 5
+  },
+  ')': {
+    value: 5
+  }
+};
 
+// Shunting Yard Algorithm
+// Takes an array as a paramater
+var shuntingYard = function shuntingYard(userInput) {
+  var outputQueue = [];
+  var operatorStack = [];
+
+  var isTopOfStackOperatorPrecedenceGreater = function isTopOfStackOperatorPrecedenceGreater(currentOperator) {
+    var value = void 0;
+    if (operatorStack.length < 1) {
+      value = 0;
+    } else {
+      value = operatorMap[operatorStack[operatorStack.length - 1]].value;
+    }
+    return value > operatorMap[currentOperator].value;
+  };
+
+  var isTopOfStackOperatorPrecedenceEqualAndSameAssociation = function isTopOfStackOperatorPrecedenceEqualAndSameAssociation(currentOperator) {
+    var value = void 0;
+    if (operatorStack.length < 1) {
+      value = 0;
+    } else {
+      value = operatorMap[operatorStack[operatorStack.length - 1]].value;
+    }
+    return value === operatorMap[currentOperator].value && operatorMap[currentOperator].association === 'left';
+  };
+
+  var operatorAtTopOfStackIsNotLeftBracket = function operatorAtTopOfStackIsNotLeftBracket() {
+    return operatorStack[operatorStack.length - 1] !== '(';
+  };
+
+  for (var i = 0; i < userInput.length; i++) {
+    var token = userInput[i];
+    if (Number(token)) {
+      outputQueue.push(token);
+    } else if (operatorMap.hasOwnProperty(token) && token !== ')' && token !== '(') {
+      while ((isTopOfStackOperatorPrecedenceGreater(token) || isTopOfStackOperatorPrecedenceEqualAndSameAssociation(token)) && operatorAtTopOfStackIsNotLeftBracket()) {
+        outputQueue.push(operatorStack.pop());
+      }
+      operatorStack.push(token);
+    } else if (token === '(') {
+      operatorStack.push(token);
+    } else if (token === ')') {
+      while (operatorAtTopOfStackIsNotLeftBracket()) {
+        outputQueue.push(operatorStack.pop());
+      }
+      operatorStack.pop();
+    }
+  }
+
+  while (operatorStack.length > 0) {
+    outputQueue.push(operatorStack.pop());
+  }
+
+  return outputQueue;
+};
+
+// Postfix Algorithm
+var postfixCalculator = function postfixCalculator(outputQueue) {
+  var stack = [];
+  for (var i = 0; i < outputQueue.length; i++) {
+    var token = outputQueue[i];
+    if (Number(token)) {
+      stack.push(token);
+    } else {
+      var rightOperand = stack.pop();
+      var leftOperand = stack.pop();
+      stack.push(operatorMap[token].operate(leftOperand, rightOperand));
+    }
+  }
+  return stack[0];
+};
 
 var inputDisplay = document.getElementById('calculator__display');
 
 document.getElementById('calculator__bottom').addEventListener('click', function (e) {
+
+  // const userInput = ['1', '+', '11', '*', '53'];
+  // const outputQueue = shuntingYard(userInput);
+  // const result = postfixCalculator(outputQueue);
   var button = e.target;
-  console.log(button);
+  buttonPop(button);
 
   if (Number(button.innerText) && inputDisplay.innerText === '0') {
     inputDisplay.innerText = button.innerText;
--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,146 @@
-// import shuntingYard from './shuntingYard';
-// import postfixCalculator from './postfix';
+// Animation Activation
+const buttonPop = (button) => {
+  button.classList.add('active');
+  setTimeout(() => {
+    button.classList.remove('active');
+  }, 750);
+};
 
-// const userInput = ['1', '+', '11', '*', '53'];
-// const outputQueue = shuntingYard(userInput);
-// const result = postfixCalculator(outputQueue);
+// Operator Map for algorithms
+const operatorMap = {
+  '^': {
+    value: 3,
+    association: 'right',
+    operate(leftOperand, rightOperand) {
+      return leftOperand ** rightOperand;
+    }
+  },
+  '*': {
+    value: 3,
+    association: 'left',
+    operate(leftOperand, rightOperand) {
+      return leftOperand * rightOperand;
+    }
+  },
+  '/': {
+    value: 3,
+    association: 'left',
+    operate(leftOperand, rightOperand) {
+      return leftOperand / rightOperand;
+    }
+  },
+  '+': {
+    value: 2,
+    association: 'left',
+    operate(leftOperand, rightOperand) {
+      return Number(leftOperand) + Number(rightOperand);
+    }
+  },
+  '-': {
+    value: 2,
+    association: 'left',
+    operate(leftOperand, rightOperand) {
+      return leftOperand - rightOperand;
+    }
+  },
+  '(': {
+    value: 5
+  },
+  ')': {
+    value: 5
+  }
+};
+
+// Shunting Yard Algorithm
+// Takes an array as a paramater
+const shuntingYard = (userInput) => {
+  const outputQueue = [];
+  const operatorStack = [];
+
+  const isTopOfStackOperatorPrecedenceGreater = (currentOperator) => {
+    let value;
+    if (operatorStack.length < 1) {
+      value = 0;
+    } else {
+      value = operatorMap[operatorStack[operatorStack.length - 1]].value;
+    }
+    return value > operatorMap[currentOperator].value;
+  };
+
+  const isTopOfStackOperatorPrecedenceEqualAndSameAssociation = (currentOperator) => {
+    let value;
+    if (operatorStack.length < 1) {
+      value = 0;
+    } else {
+      value = operatorMap[operatorStack[operatorStack.length - 1]].value;
+    }
+    return (value === operatorMap[currentOperator].value &&
+      operatorMap[currentOperator].association === 'left');
+  };
+
+  const operatorAtTopOfStackIsNotLeftBracket = () => (
+    operatorStack[operatorStack.length - 1] !== '('
+  );
+
+  for (let i = 0; i < userInput.length; i++) {
+    const token = userInput[i];
+    if (Number(token)) {
+      outputQueue.push(token);
+    } else if (
+      (operatorMap.hasOwnProperty(token) &&
+      token !== ')' && token !== '(')
+    ) {
+      while (
+        (isTopOfStackOperatorPrecedenceGreater(token) ||
+          isTopOfStackOperatorPrecedenceEqualAndSameAssociation(token))
+        && operatorAtTopOfStackIsNotLeftBracket()
+      ) {
+        outputQueue.push(operatorStack.pop());
+      }
+      operatorStack.push(token);
+    } else if (token === '(') {
+      operatorStack.push(token);
+    } else if (token === ')') {
+      while (operatorAtTopOfStackIsNotLeftBracket()) {
+        outputQueue.push(operatorStack.pop());
+      }
+      operatorStack.pop();
+    }
+  }
+
+  while (operatorStack.length > 0) {
+    outputQueue.push(operatorStack.pop());
+  }
+
+  return outputQueue;
+};
+
+// Postfix Algorithm
+const postfixCalculator = (outputQueue) => {
+  const stack = [];
+  for (let i = 0; i < outputQueue.length; i++) {
+    const token = outputQueue[i];
+    if (Number(token)) {
+      stack.push(token);
+    } else {
+      const rightOperand = stack.pop();
+      const leftOperand = stack.pop();
+      stack.push(operatorMap[token].operate(leftOperand, rightOperand));
+    }
+  }
+  return stack[0];
+};
 
 
 const inputDisplay = document.getElementById('calculator__display');
 
 document.getElementById('calculator__bottom').addEventListener('click', (e) => {
+
+// const userInput = ['1', '+', '11', '*', '53'];
+// const outputQueue = shuntingYard(userInput);
+// const result = postfixCalculator(outputQueue);
   let button = e.target
-  console.log(button)
+  buttonPop(button);
 
   if (Number(button.innerText) && inputDisplay.innerText === '0') {
     inputDisplay.innerText = button.innerText;
