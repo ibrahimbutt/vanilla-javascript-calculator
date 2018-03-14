c50ad15b7b0b26f97b2bdc29c306df119c5193c0
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2018-03-14 18:35:30 +0000

    Save

--- dist/main.js
+++ dist/main.js
@@ -1,78 +1,17 @@
 'use strict';
 
-var _calculate = require('./calculate');
-
-var _calculate2 = _interopRequireDefault(_calculate);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var thiss = ['1', '+', '2'];
-console.log(1123);
-
-var state = {
-  store: [],
-  operatorLastPressed: false
-};
-
 var handlers = {
-  onDigitPress: function onDigitPress(buttonPressed) {
-    if (inputDisplay.textContent === '0' && buttonPressed !== '.' || state.operatorLastPressed) {
-      view.setDisplay(buttonPressed);
-    } else if (inputDisplay.textContent === '0' && buttonPressed === '.') {
-      // Use set display, to acoid uncessesary steps in addToDisplay().
-      // More to the point.
-      view.setDisplay('0' + buttonPressed);
-    } else if (inputDisplay.textContent.includes('.') && buttonPressed === '.') {
-      return false;
-    } else if (buttonPressed === '.') {
-      // toLocaleString will remove decimal
-      // E.g. if inputDisplay is 56 button press is a decimal
-      // format() will remove trailing decimal point.
-      view.setDisplay(inputDisplay.textContent + buttonPressed);
-    } else {
-      view.addToDisplay(buttonPressed);
-    }
-    state.operatorLastPressed = false;
+  removeFormatting: function removeFormatting(input) {
+    return input.includes('e') ? Number(input).toString() : input.replace(/,/g, '');
   },
-  onOperatorPress: function onOperatorPress(buttonPressed) {
-    if (inputDisplay.textContent === '0') {
-      return false;
-    } else if (state.operatorLastPressed) {
-      state.store.pop();
-      state.store.push(buttonPressed);
-    } else {
-      this.calculateTotal();
-      state.store.push(buttonPressed);
-    }
-    state.operatorLastPressed = true;
+  addFormatting: function addFormatting(input) {
+    return input.length > 9 ? Number(input).toExponential().toString() : input.toLocaleString();
   },
-  format: function format(value) {
-    if (value.includes(',') && value.replace(/,/g, '').length >= 9) {
-      return String(Number(value.replace(/[,]/g, '')).toExponential(3)).replace(/\+/, '');
-    } else if (Number(value) > 999999999) {
-      return String(Number(value).toExponential(3)).replace(/\+/, '');
-    } else {
-      return String(Number(value.replace(/,/g, '')).toLocaleString());
-    }
-  },
-  onPlusMinusPress: function onPlusMinusPress() {
-    Number(inputDisplay.textContent.replace(/,/g, '')) > 0 ? view.setDisplay('-' + inputDisplay.textContent) : view.setDisplay(inputDisplay.textContent.replace(/-/, ''));
-  },
-  onPercentPress: function onPercentPress() {
-    view.setDisplay(this.format(String(inputDisplay.textContent.replace(/,/g, '') / 100)));
-  },
-  calculateTotal: function calculateTotal() {
-    state.store.push(String(Number(inputDisplay.textContent.replace(/,/g, ''))));
-    var outputQueue = shuntingYard(state.store);
-    state.store = [postfixCalculator(outputQueue)];
-    view.setDisplay(state.store[0]);
-  },
-  clearAll: function clearAll() {
-    state.store = [];
-    state.operatorLastPressed = false;
-    view.setDisplay('0');
+  onDigitPress: function onDigitPress(input, buttonPressed) {
+    return input + buttonPressed;
   }
 };
+
 var view = {
   buttonAnimation: function buttonAnimation(buttonPressed) {
     buttonPressed.classList.add('active');
@@ -82,14 +21,6 @@ var view = {
   },
   updateChromaticEffect: function updateChromaticEffect() {
     inputDisplay.setAttribute("node-content", inputDisplay.textContent);
-  },
-  setDisplay: function setDisplay(buttonPressed) {
-    inputDisplay.textContent = buttonPressed;
-  },
-  addToDisplay: function addToDisplay(buttonPressed) {
-    var newDisplayText = inputDisplay.textContent.includes('e') ? Number(inputDisplay.textContent) + buttonPressed : inputDisplay.textContent.replace(/,/g, '') + buttonPressed;
-    var newDisplayTextFormatted = handlers.format(newDisplayText);
-    this.setDisplay(newDisplayTextFormatted);
   }
 };
 
@@ -97,22 +28,27 @@ var inputDisplay = document.getElementById('calculator__display');
 
 document.getElementById('calculator__bottom').addEventListener('click', function (e) {
   var buttonPressed = e.target.textContent;
+  var input = handlers.removeFormatting(inputDisplay.textContent);
+  var newInput = void 0;
 
   if (Number(buttonPressed) || buttonPressed === '.' || buttonPressed === '0') {
-    handlers.onDigitPress(buttonPressed);
-  } else if (buttonPressed === '+/-') {
-    handlers.onPlusMinusPress();
-  } else if (buttonPressed === '%') {
-    handlers.onPercentPress();
-  } else if (buttonPressed === 'AC') {
-    handlers.clearAll();
-  } else if (buttonPressed === '=') {
-    handlers.calculateTotal();
-  } else {
-    handlers.onOperatorPress(buttonPressed);
+    // handlers.onDigitPress(buttonPressed);
+    newInput = handlers.onDigitPress(input, buttonPressed);
   }
+  // else if (buttonPressed === '+/-') {
+  //   handlers.onPlusMinusPress();
+  // } else if (buttonPressed === '%') {
+  //   handlers.onPercentPress();
+  // } else if (buttonPressed === 'AC') {
+  //   handlers.clearAll();
+  // } else if (buttonPressed === '=') {
+  //   handlers.calculateTotal();
+  // } else {
+  //   handlers.onOperatorPress(buttonPressed);
+  // }
 
-  handlers.format(inputDisplay.textContent);
+  input = handlers.addFormatting(newInput);
+  inputDisplay.textContent = input;
   view.buttonAnimation(e.target);
   view.updateChromaticEffect();
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,79 +1,19 @@
-import calculate from "./calculate";
-
-const thiss = ['1', '+', '2'];
-console.log(1123);
-
-const state = {
-  store: [],
-  operatorLastPressed: false
-};
-
 const handlers = {
-  onDigitPress(buttonPressed) {
-    if (
-      (inputDisplay.textContent === '0' && buttonPressed !== '.') ||
-      state.operatorLastPressed
-    ) {
-      view.setDisplay(buttonPressed);
-    } else if (inputDisplay.textContent === '0' && buttonPressed === '.') {
-      // Use set display, to acoid uncessesary steps in addToDisplay().
-      // More to the point.
-      view.setDisplay('0' + buttonPressed);
-    } else if (inputDisplay.textContent.includes('.') && buttonPressed === '.') {
-      return false;
-    } else if (buttonPressed === '.') {
-      // toLocaleString will remove decimal
-      // E.g. if inputDisplay is 56 button press is a decimal
-      // format() will remove trailing decimal point.
-      view.setDisplay( inputDisplay.textContent + buttonPressed);
-    } else {
-      view.addToDisplay(buttonPressed);
-    }
-    state.operatorLastPressed = false;
+  removeFormatting(input) {
+    return input.includes('e') ?
+      Number(input).toString() :
+      input.replace(/,/g, '');
   },
-  onOperatorPress(buttonPressed) {
-    if (inputDisplay.textContent === '0') {
-      return false;
-    } else if (state.operatorLastPressed) {
-      state.store.pop();
-      state.store.push(buttonPressed);
-    } else {
-      this.calculateTotal();
-      state.store.push(buttonPressed)
-    }
-    state.operatorLastPressed = true;
+  addFormatting(input) {
+    return input.length > 9 ?
+      Number(input).toExponential().toString() :
+      input.toLocaleString();
   },
-  format(value) {
-    if (value.includes(',') && value.replace(/,/g, '').length >= 9) {
-      return String(Number(value.replace(/[,]/g, '')).toExponential(3)).replace(/\+/, '');
-    } else if (Number(value) > 999999999) {
-      return String(Number(value).toExponential(3)).replace(/\+/, '');
-    } else {
-      return String(Number(value.replace(/,/g, '')).toLocaleString());
-    }
-  },
-  onPlusMinusPress() {
-    (Number(inputDisplay.textContent.replace(/,/g, '')) > 0 ?
-      view.setDisplay('-' + inputDisplay.textContent) :
-      view.setDisplay(inputDisplay.textContent.replace(/-/, '')));
-  },
-  onPercentPress() {
-    view.setDisplay(
-      this.format(String(inputDisplay.textContent.replace(/,/g, '') / 100))
-    );
-  },
-  calculateTotal() {
-    state.store.push(String(Number(inputDisplay.textContent.replace(/,/g, ''))));
-    const outputQueue = shuntingYard(state.store);
-    state.store = [postfixCalculator(outputQueue)];
-    view.setDisplay(state.store[0])
-  },
-  clearAll() {
-    state.store = [];
-    state.operatorLastPressed = false;
-    view.setDisplay('0');
+  onDigitPress(input, buttonPressed) {
+    return input + buttonPressed;
   }
 };
+
 const view = {
   buttonAnimation(buttonPressed) {
     buttonPressed.classList.add('active');
@@ -84,39 +24,33 @@ const view = {
   updateChromaticEffect() {
     inputDisplay.setAttribute("node-content", inputDisplay.textContent);
   },
-  setDisplay(buttonPressed) {
-    inputDisplay.textContent = buttonPressed;
-  },
-  addToDisplay(buttonPressed) {
-    let newDisplayText = (inputDisplay.textContent.includes('e') ?
-      Number(inputDisplay.textContent) + buttonPressed :
-      inputDisplay.textContent.replace(/,/g, '') + buttonPressed);
-    const newDisplayTextFormatted = handlers.format(newDisplayText)
-    this.setDisplay(newDisplayTextFormatted);
-  }
-};
+}
 
 const inputDisplay = document.getElementById('calculator__display');
 
 document.getElementById('calculator__bottom').addEventListener('click', (e) => {
   const buttonPressed = e.target.textContent;
+  let input = handlers.removeFormatting(inputDisplay.textContent);
+  let newInput;
 
   if (Number(buttonPressed) || buttonPressed === '.' || buttonPressed === '0') {
-    handlers.onDigitPress(buttonPressed);
-  } else if (buttonPressed === '+/-') {
-    handlers.onPlusMinusPress();
-  } else if (buttonPressed === '%') {
-    handlers.onPercentPress();
-  } else if (buttonPressed === 'AC') {
-    handlers.clearAll();
-  } else if (buttonPressed === '=') {
-    handlers.calculateTotal();
-  } else {
-    handlers.onOperatorPress(buttonPressed);
+    // handlers.onDigitPress(buttonPressed);
+    newInput = handlers.onDigitPress(input, buttonPressed)
   }
+  // else if (buttonPressed === '+/-') {
+  //   handlers.onPlusMinusPress();
+  // } else if (buttonPressed === '%') {
+  //   handlers.onPercentPress();
+  // } else if (buttonPressed === 'AC') {
+  //   handlers.clearAll();
+  // } else if (buttonPressed === '=') {
+  //   handlers.calculateTotal();
+  // } else {
+  //   handlers.onOperatorPress(buttonPressed);
+  // }
 
-  handlers.format(inputDisplay.textContent);
+  input = handlers.addFormatting(newInput);
+  inputDisplay.textContent = input;
   view.buttonAnimation(e.target)
   view.updateChromaticEffect();
 });
-
