2a0f19efb031419c80255d389ae59c0676157f96
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2018-03-27 19:20:35 +0100

    Fix Formatting Bug

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,72 +1,76 @@
-import state from './state';
-import { shuntingYard, postfixCalculator } from './calculate';
+import state from "./state";
+import { shuntingYard, postfixCalculator } from "./calculate";
 
 const handlers = {
   clearAll(inputDisplay) {
     inputDisplay.innerText = 0;
-    state.store = []
+    state.store = [];
     state.operatorLastPressed = false;
   },
   removeFormatting(input) {
-    return input.includes('e') ?
-      Number(input).toString() :
-      input.replace(/,/g, '');
+    return input.includes("e")
+      ? Number(input).toString()
+      : input.replace(/,/g, "");
   },
   addFormatting(input) {
-    return input > 999999999 ?
-      Number(input).toExponential(2).toString() :
-      this.addCommas(input);
+    return input > 999999999
+      ? Number(input)
+          .toExponential(2)
+          .toString()
+      : this.addCommas(input);
   },
   addCommas(input) {
     let inlcudesDecimal = false;
     let afterDecimalPortionOfValue;
-    if (input.includes('.')) {
+    if (input.includes(".")) {
       afterDecimalPortionOfValue = input
-        .split('')
-        .map((x) => {
-          if (x === '.') {
+        .split("")
+        .map(x => {
+          if (x === ".") {
             inlcudesDecimal = true;
             return x;
           } else if (inlcudesDecimal) {
             return x;
           }
         })
-        .join('');
+        .join("");
 
-      afterDecimalPortionOfValue = afterDecimalPortionOfValue === '.' ?
-        '.' :
-        Number(afterDecimalPortionOfValue).toFixed(
-          afterDecimalPortionOfValue.length > 2 ? 2 : 1
-        ).toString().replace(/^0/, '');
+      afterDecimalPortionOfValue =
+        afterDecimalPortionOfValue === "."
+          ? "."
+          : Number(afterDecimalPortionOfValue)
+              .toFixed(afterDecimalPortionOfValue.length > 2 ? 2 : 1)
+              .toString()
+              .replace(/^0/, "");
     }
 
-    const valueWithCommas = input
-      // Remove portion after decimals
-      .replace(/\..*/, '')
-      .split('')
-      .reverse()
-      .map((x, i) => {
-        return (i % 3 === 0 ? x + ',' : x);
-      })
-      .reverse()
-      .join('')
-      // Remove extra comma
-      .slice(0, -1);
+    let valueWithCommas;
+    if (input.length >= 3) {
+      const valueWithCommas = input
+        // Remove portion after decimals
+        .replace(/\..*/, "")
+        .split("")
+        .reverse()
+        .map((x, i) => {
+          return i % 3 === 0 ? x + "," : x;
+        })
+        .reverse()
+        .join("")
+        // Remove extra comma
+        .slice(0, -1);
+    }
 
-    return valueWithCommas + (afterDecimalPortionOfValue || '')
+    return (valueWithCommas  || input) + (afterDecimalPortionOfValue || "");
   },
   onDigitPress(input, buttonPressed) {
-    if (
-      (input === '0' && buttonPressed !== '.') ||
-      state.operatorLastPressed
-    ) {
+    if ((input === "0" && buttonPressed !== ".") || state.operatorLastPressed) {
       state.operatorLastPressed = false;
       return buttonPressed;
-    } else if (input === '0' && buttonPressed === '.') {
-      console.log(123)
+    } else if (input === "0" && buttonPressed === ".") {
+      console.log(123);
       state.operatorLastPressed = false;
-      return '0.'
-    } else if (buttonPressed === '.' && input.includes('.')) {
+      return "0.";
+    } else if (buttonPressed === "." && input.includes(".")) {
       return false;
     } else {
       state.operatorLastPressed = false;
@@ -74,7 +78,7 @@ const handlers = {
     }
   },
   onOperatorPress(input, buttonPressed) {
-    if (input === '0') {
+    if (input === "0") {
       return false;
     } else if (state.operatorLastPressed) {
       state.store.pop();
@@ -90,18 +94,18 @@ const handlers = {
     }
   },
   onPlusMinusPress(input) {
-    return input > 0 ? '-' + input : input.replace(/^-/, '');
+    return input > 0 ? "-" + input : input.replace(/^-/, "");
   },
   onPercentPress(input) {
     return (input / 100).toString();
   },
   calculateTotal(input) {
     state.store.push(input);
-    console.log(state.store)
+    console.log(state.store);
     const userInput = state.store;
     const outputQueue = shuntingYard(userInput);
     state.store = [postfixCalculator(outputQueue)];
-    console.log(state.store[0])
+    console.log(state.store[0]);
     return state.store[0];
   }
 };
